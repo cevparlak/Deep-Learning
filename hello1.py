156511cacf1deb4f93a5213e1662ee489702ca8c
cevparlak <45640286+cevparlak@users.noreply.github.com>
2019-01-20 15:16:34 +0300

    Delete hello1.py

--- hello1.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-#Create a constant that contains a string.
-hello = tf.constant('Hello, TensorFlow!')
-#Create a TensorFlow session.
-sess = tf.Session()
-#You can ignore the warnings that the TensorFlow library wasn't compiled to use certain instructions.
-#Display the value of hello.
-print(sess.run(hello))
